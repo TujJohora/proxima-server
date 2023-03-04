f4b42f154aea30999e49cd1c1fdacde2bb3729f9
TujJohora <tujjohoraa@gmail.com>
2023-03-04 21:56:39 +0600

    route

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ const app = express();
 
 //VARIABLE FOR PORT 
 const port=process.env.PORT|| 4000;
+
+
+//routes
+app.get('/',(req,res)=>{
+    res.json({message:"welcome the express app!"});
+});
+
 //listen for requests
 app.listen(port,()=>{
     console.log(`Listening on port ${port}`);
