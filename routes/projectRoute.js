ab439170d80d4178f0144654a08c51760667e594
TujJohora <tujjohoraa@gmail.com>
2023-03-04 23:35:38 +0600

    diff router added

--- routes/projectRoute.js
+++ routes/projectRoute.js
@@ -5,8 +5,35 @@ const express = require("express");
 //R should be capital
 const router = express.Router();
 
-//routes
-router.get("/",()=>{});
+// GET all projects
+router.get("/",(req, res) =>{
+    res.json({message:"GET all projects"});
+    
+});
 
+// GET a single project
+
+router.get("/",(req, res) =>{
+    res.json({message:"GET a single project"});
+    
+});
+// POST a new project
+router.post("/",(req, res) =>{
+    res.json({message:"POST a new project"});
+    
+});
+
+// DELETE a project
+router.delete("/",(req, res) =>{
+    res.json({message:"DELETE a project"});
+    
+});
+
+// UPDATE a project
+
+router.patch("/:id",(req, res) =>{
+    res.json({message:"UPDATE a project"});
+    
+});
 //exporting router
 module.exports = router;
