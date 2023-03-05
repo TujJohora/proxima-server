7a6ec23a7d05f25f8cda82c3dbc00df908610d72
TujJohora <tujjohoraa@gmail.com>
2023-03-05 16:59:22 +0600

    jsonwebtoken added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,9 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
 
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.SECRET, { expiresIn: "7d" });
+};
 // login user
 const loginUser = async (req, res) => {
   res.json({ message: "login" });
@@ -12,7 +16,10 @@ const signupUser = async (req, res) => {
   try {
     const user = await User.signup(email, password);
 
-    res.status(200).json(user);
+    // create token
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
